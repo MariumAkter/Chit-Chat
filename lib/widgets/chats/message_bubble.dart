49918c4432e1377d5a5f10738933b8580f07abdf
Mariumchonmon <Mariumakterch@gmail.com>
2021-08-23 00:01:06 +0600

    FutureBuilder<DocumentSnapshot> work

--- lib/widgets/chats/message_bubble.dart
+++ lib/widgets/chats/message_bubble.dart
@@ -1,20 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 class MessageBubble extends StatelessWidget {
   MessageBubble(
       this.message,
+      this.userId,
       this.isMe,
+      {required this.key}
       );
+  final Key key;
   final String message;
+  final String userId;
   final bool isMe;
+
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: <Widget> [
         Container(
           decoration: BoxDecoration(
-            color: Theme.of(context).accentColor,
-            borderRadius: BorderRadius.circular(12),
+            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft: isMe ? Radius.circular(0) : Radius.circular(12),
+              bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+            ),
           ),
           width: 140,
           padding: EdgeInsets.symmetric(
@@ -25,11 +37,28 @@ class MessageBubble extends StatelessWidget {
             vertical: 4,
             horizontal: 8,
           ),
-          child: Text(
-              message,
-            style: TextStyle(
-              color: Theme.of(context).accentTextTheme.title!.color,
-            ),
+          child: Column(
+            children: [
+              FutureBuilder<DocumentSnapshot>(
+                future: FirebaseFirestore.instance.collection('users').doc(userId).get(),
+                builder: (context, snapshot) {
+                  if(snapshot.connectionState == ConnectionState.waiting){
+                    return Text('Loading...');
+                  }
+                  return Text(
+                    snapshot.data!['username'],
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold),
+                  );
+                }
+              ),
+              Text(
+                  message,
+                style: TextStyle(
+                  color: isMe ? Colors.black : Theme.of(context).accentTextTheme.title!.color,
+                ),
+              ),
+            ],
           ),
         ),
       ],
--- lib/widgets/chats/messages.dart
+++ lib/widgets/chats/messages.dart
@@ -3,33 +3,43 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'message_bubble.dart';
 
+
 class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection('chat')
-          .orderBy(
-          'createdAt',
-        descending: true,
-      ).snapshots() ,
-      builder: (ctx, chatSnapshot) {
-        if (chatSnapshot.connectionState == ConnectionState.waiting){
-          return Center(child:  CircularProgressIndicator(),);
-        }
+    return  FutureBuilder(
+        future: Future.value(FirebaseAuth.instance.currentUser),
+        builder: (ctx, futureSnapshot){
+          if(futureSnapshot.connectionState == ConnectionState.waiting){
+            return Center(child: CircularProgressIndicator(),);
+          }
+       return StreamBuilder(
+            stream: FirebaseFirestore.instance.collection('chat')
+             .orderBy(
+             'createdAt',
+            descending: true,
+            ).snapshots() ,
+            builder: (ctx, chatSnapshot) {
+           if (chatSnapshot.connectionState == ConnectionState.waiting){
+           return Center(child:  CircularProgressIndicator(),);
+         }
         final chatDocs = (chatSnapshot.data as QuerySnapshot).docs;
-        return FutureBuilder(
-          future: Future.value(FirebaseAuth.instance.currentUser),
-          builder: (ctx, futureSnapshot) => ListView.builder(
-            reverse: true,
-            itemCount: chatDocs.length,
-              itemBuilder: (ctx, index) => MessageBubble(
-                chatDocs[index]['text'],
-                chatDocs[index]['userId'],
-              ),
+        return ListView.builder(
+              reverse: true,
+              itemCount: chatDocs.length,
+              itemBuilder: (ctx, index) =>
+                  MessageBubble(
+                    chatDocs[index]['text'],
+                    chatDocs[index]['userId'],
+                    chatDocs[index]['userId'] == (futureSnapshot.data as User).uid,
+                    key: ValueKey(chatDocs[index].id),
+                  ),
 
-          ),
+            );
+          }
         );
-      },);
+      },
+      );
   }
 }
